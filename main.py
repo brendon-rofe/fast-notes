062421e8012968262389dbd2e577b06e4b84c5d8
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-01 14:02:05 +0200

    feat: Implement get note by id functionality

--- main.py
+++ main.py
@@ -14,6 +14,13 @@ with open("notes_data.json") as file:
 
 app = FastAPI()
 
-@app.get("/all")
-def all_notes():
+@app.get("/notes")
+def get_all():
   return notes
+
+@app.get("/notes/{note_id}")
+def get_by_id(note_id: int):
+  for note in notes:
+    if note["id"] == note_id:
+      return note
+  return {"Error": "No note with that ID found"}
