1954e5b227306eecc5957db12d366cd688f9fc09
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-01 13:23:47 +0200

    feat: Implement notes data saving in json

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+import json
 
 notes = []
+
+with open("notes_data.json") as file:
+  notes = json.load(file)
 
 app = FastAPI()
 
 @app.get("/all")
 def all_notes():
-    return notes
+  return notes
