7eccee0e346f94f9305be33d867e169ee694e385
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-02 08:49:13 +0200

    feat: Implement create note functionality

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 import json
 
 class Note(BaseModel):
-  id: int
+  id: Optional[str] = None
   title: str
   content: str
 
@@ -24,3 +25,16 @@ def get_by_id(note_id: int):
     if note["id"] == note_id:
       return note
   return {"Error": "No note with that ID found"}
+
+@app.post("/notes")
+def create(note: Note):
+	id = len(notes) + 1
+	new_note = {
+		"id": id,
+		"title": note.title,
+		"content": note.content
+	}
+	notes.append(new_note)
+	with open("notes_data.json", "w") as file:
+		json.dump(notes, file, indent=2)
+
