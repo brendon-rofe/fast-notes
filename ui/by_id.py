48412dbf131cb6d9c44fdff528f26c2349f2b878
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-03 11:54:04 +0200

    fix: Set number of notes and refactor

--- ui/by_id.py
+++ ui/by_id.py
@@ -1,19 +1,22 @@
 import streamlit as st
 import requests
 
+all_notes = requests.get(
+  "http://localhost:8000/notes"
+)
+
+num_notes = len(all_notes.json())
+
 def get_by_id(note_id):
   response = requests.get(
     f"http://localhost:8000/notes/{note_id}"
   )
   note = response.json()
-  if "title" in note:
-    st.subheader(f"{note["title"]}")
-    st.write(f"{note["content"]}")
-  else:
-    st.warning("No note with that ID found")
+  st.subheader(f"{note["title"]}")
+  st.write(f"{note["content"]}")
 
 st.header("Find Note By ID:")
 
-note_id = st.slider("Pick a note ID", 0, 5)
+note_id = st.slider("Pick a note ID", 1, num_notes)
 
 get_by_id(note_id)
