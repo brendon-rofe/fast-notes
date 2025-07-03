9efc332007d856550e1c019be500b45062694b53
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-03 14:04:58 +0200

    fix: Refactor to use spinner

--- ui/by_id.py
+++ ui/by_id.py
@@ -1,22 +1,27 @@
 import streamlit as st
 import requests
 
-all_notes = requests.get(
-  "http://localhost:8000/notes"
-)
-
-num_notes = len(all_notes.json())
-
-def get_by_id(note_id):
-  response = requests.get(
-    f"http://localhost:8000/notes/{note_id}"
-  )
-  note = response.json()
-  st.subheader(f"{note["title"]}")
-  st.write(f"{note["content"]}")
-
 st.header("Find Note By ID:")
 
-note_id = st.slider("Pick a note ID", 1, num_notes)
+note_id = None
+
+with st.spinner("Getting number of notes..."):
+  response = requests.get(
+  "http://localhost:8000/notes"
+  )
+  if response.status_code == 200:
+    num_notes = len(response.json())
+
+    note_id = st.slider("Pick a note ID", 1, num_notes)
+
+def get_by_id(note_id):
+  with st.spinner("Loading note..."):
+    response = requests.get(
+      f"http://localhost:8000/notes/{note_id}"
+    )
+    if response.status_code == 200:
+      note = response.json()
+      st.subheader(f"{note["title"]}")
+      st.write(f"{note["content"]}")
 
 get_by_id(note_id)
