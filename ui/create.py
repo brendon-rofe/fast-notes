122b8eb0458ce6afb2e0d90191398612f0cde624
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-03 13:47:40 +0200

    fix: add spinner to indicate that the note is being created

--- ui/create.py
+++ ui/create.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.header("Create a note:")
 
@@ -9,4 +10,16 @@ with st.form("create_note_form"):
   submitted = st.form_submit_button("Submit")
   
 if submitted:
-    st.success("New note created!")
+  note = {
+    "title": note_title,
+    "content": note_content
+  }
+  with st.spinner("Creating note..."):
+    response = requests.post(
+      "http://localhost:8000/notes",
+      json=note
+    )
+    if response.status_code == 200:
+      st.success("Note created successfully!")
+    else:
+      st.error("Failed to create new note")
