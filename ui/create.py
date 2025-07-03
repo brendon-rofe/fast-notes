5844f3a506a781bfaf685057603a00c6adfc53e9
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-03 13:28:59 +0200

    fix: Refactor

--- ui/create.py
+++ ui/create.py
@@ -3,9 +3,10 @@ import streamlit as st
 st.header("Create a note:")
 
 with st.form("create_note_form"):
-  note_title = st.text_input("Your note's title:")
-  note_content = st.text_area("Your note's content")
+  note_title = st.text_input("Title:")
+  note_content = st.text_area("Content")
   
   submitted = st.form_submit_button("Submit")
-  if submitted:
+  
+if submitted:
     st.success("New note created!")
