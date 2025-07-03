36627fe95380f4e0b5dc91d23061aa5fec799d79
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-03 11:42:16 +0200

    feat: Implement finding note by ID

--- ui/by_id.py
+++ ui/by_id.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import requests
 
+def get_by_id(note_id):
+  response = requests.get(
+    f"http://localhost:8000/notes/{note_id}"
+  )
+  note = response.json()
+  if "title" in note:
+    st.subheader(f"{note["title"]}")
+    st.write(f"{note["content"]}")
+  else:
+    st.warning("No note with that ID found")
 
+st.header("Find Note By ID")
+
+note_id = st.slider("Pick a note ID", 0, 5)
+
+get_by_id(note_id)
