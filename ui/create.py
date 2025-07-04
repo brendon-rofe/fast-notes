33bac51d9c6d4932393c0b7234fd867f75313daf
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2025-07-04 13:37:38 +0200

    fix: Add basic form validation

--- ui/create.py
+++ ui/create.py
@@ -10,16 +10,19 @@ with st.form("create_note_form"):
   submitted = st.form_submit_button("Submit")
   
 if submitted:
-  note = {
-    "title": note_title,
-    "content": note_content
-  }
-  with st.spinner("Creating note..."):
-    response = requests.post(
-      "http://localhost:8000/notes",
-      json=note
-    )
-    if response.status_code == 200:
-      st.success("Note created successfully!")
-    else:
-      st.error("Failed to create new note")
+  if not note_title.strip() or not note_content.strip():
+    st.error("Both Title and Content are required.")
+  else:
+    note = {
+      "title": note_title,
+      "content": note_content
+    }
+    with st.spinner("Creating note..."):
+      response = requests.post(
+        "http://localhost:8000/notes",
+        json=note
+      )
+      if response.status_code == 200:
+        st.success("Note created successfully!")
+      else:
+        st.error("Failed to create new note")
